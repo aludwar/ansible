5834a4995736905e2d73ada86ba2d12a9d4bbdbe
Andrew Ludwar <andrew.ludwar@gmail.com>
2018-08-30 08:17:59 -0600

    Changed to use user module for idempotency

--- roles/base/templates/set-aludwar.sh
+++ roles/base/templates/set-aludwar.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-useradd -p '$6$/xFdPAJvqAbyB/I7$KPqseBHmBZJ4qRW09hYnO8uX.GEyqga/3DaelNzTT0ZahpdAX2k/4hphXcz24lgID7VgGM1FZ1vnr7obAU09w1' aludwar
-echo "aludwar ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/aludwar
-chmod 0440 /etc/sudoers.d/aludwar
 wget http://10.0.0.4/aludwar.tar && tar -xvf aludwar.tar -C /home/aludwar
